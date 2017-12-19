bd76056659a3ee1ffa7190d16fa5189cc4e705b7
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2017-12-18 22:40:03 -0600

    Added base logic to generate minefield

---
+++ src/Components/Game/index.js
@@ -0,0 +1,47 @@
+import Client from 'boardgame.io/client';
+import Game from 'boardgame.io/game';
+
+import MineBoard from '../Board';
+
+const boardsize = 4;
+const mines = 8;
+
+const generateMinefield = size => {
+  const sizeWithBorder = (size + 2) ** 2;
+  const emptyCells = Array(sizeWithBorder).fill({
+    id: null,
+    hasBomb: false,
+    isBorder: false,
+    isRevealed: false,
+  });
+
+  return emptyCells.map((cell, i) => {
+    const isBorder =
+      i < size + 2 ||
+      i >= sizeWithBorder - (size + 2) ||
+      i % (size + 2) === 0 ||
+      (i + 1) % (size + 2) === 0;
+
+    return {
+      ...cell,
+      id: i,
+      isBorder,
+    };
+  });
+};
+
+const MineSweeper = Game({
+  G: {
+    cells: generateMinefield(boardsize),
+    boardsize,
+    mines,
+  },
+
+  moves: {
+    clickCell(G, ctx, id) {},
+  },
+});
+
+const MineSweeperGame = Client({ game: MineSweeper, board: MineBoard });
+
+export default MineSweeperGame;
